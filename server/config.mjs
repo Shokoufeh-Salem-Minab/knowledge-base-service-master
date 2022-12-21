c70b72119fd0fdccb375bdda7cf8937938460b65
Shokoufeh-Salem-Minab <109625980+Shokoufeh-Salem-Minab@users.noreply.github.com>
2022-12-21 11:39:48 +0200

    Config parameters description

--- server/config.mjs
+++ server/config.mjs
@@ -1,9 +1,9 @@
 const config = {
-    hostname: '127.0.0.1',
-    port: 3333,
-    database: 'data/db.json',
-    defaultTrustLevel: 0.5,
-    trustLevelThreshold: 0.1,
-    trustLevelStep: 0.15
+    hostname: '127.0.0.1', // server hostname
+    port: 3333, // server port
+    database: 'data/db.json', // knowledge base location path relative to /server folder
+    defaultTrustLevel: 0.5, // default trust level for new name added to DB
+    trustLevelThreshold: 0.1, // trust level threshold below what name will be deleted from DB
+    trustLevelStep: 0.15 // trust level increasing or decreasing step when updating knowledge base
 }
 export {config};
